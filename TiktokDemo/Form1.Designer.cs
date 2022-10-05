9b07d8a6f778f97706ab9c6141de3b303e41ab64
Administrator <11@qq.com>
2022-10-05 23:26:54 +0800

    添加项目文件。

---
+++ TiktokDemo/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace TiktokDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtURL = new System.Windows.Forms.TextBox();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.chromiumWebBrowser1 = new CefSharp.WinForms.ChromiumWebBrowser();
+            this.btnParse = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // txtURL
+            // 
+            this.txtURL.Location = new System.Drawing.Point(13, 3);
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(414, 21);
+            this.txtURL.TabIndex = 0;
+            this.txtURL.Text = "https://www.tiktok.com/@ofeliavietnam";
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(433, 1);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(65, 23);
+            this.btnGo.TabIndex = 1;
+            this.btnGo.Text = "解析";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnParse_Click);
+            // 
+            // chromiumWebBrowser1
+            // 
+            this.chromiumWebBrowser1.ActivateBrowserOnCreation = false;
+            this.chromiumWebBrowser1.Location = new System.Drawing.Point(4, 31);
+            this.chromiumWebBrowser1.Name = "chromiumWebBrowser1";
+            this.chromiumWebBrowser1.Size = new System.Drawing.Size(59, 435);
+            this.chromiumWebBrowser1.TabIndex = 2;
+            // 
+            // btnParse
+            // 
+            this.btnParse.Location = new System.Drawing.Point(648, 3);
+            this.btnParse.Name = "btnParse";
+            this.btnParse.Size = new System.Drawing.Size(107, 23);
+            this.btnParse.TabIndex = 3;
+            this.btnParse.Text = "解析";
+            this.btnParse.UseVisualStyleBackColor = true;
+            this.btnParse.Visible = false;
+            this.btnParse.Click += new System.EventHandler(this.btnParse_Click_1);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(69, 30);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(429, 436);
+            this.listBox1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(512, 472);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnParse);
+            this.Controls.Add(this.chromiumWebBrowser1);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.txtURL);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ticktok ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtURL;
+        private System.Windows.Forms.Button btnGo;
+        private CefSharp.WinForms.ChromiumWebBrowser chromiumWebBrowser1;
+        private System.Windows.Forms.Button btnParse;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ TiktokDemo/Form1.cs
@@ -0,0 +1,183 @@
+﻿using CefSharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TiktokDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnParse_Click(object sender, EventArgs e)
+        {
+            string  url = txtURL.Text;
+            chromiumWebBrowser1.LoadUrl(url);
+
+            Thread.Sleep(1000 * 10);
+
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("function tempFunction() {");
+            sb.AppendLine("     var arry=[];");
+            //sb.AppendLine("     var obj=document.getElementsByClassName('tiktok-1mo2fkg-DivUserLinkContainer e797se20');");
+            sb.AppendLine("     var obj=document.getElementsByClassName('tiktok-rfcpgv-PUserDesc e797se29');");
+            
+            sb.AppendLine("  for (var i in obj){ arry.push(obj[i].innerHTML); }");
+
+
+            sb.AppendLine("     return arry;");
+            sb.AppendLine("}");
+            sb.AppendLine("tempFunction();");
+
+            
+
+
+            var task = chromiumWebBrowser1.EvaluateScriptAsync(sb.ToString());
+            task.ContinueWith(t =>
+            {
+                if (!t.IsFaulted)
+                {
+                    var response = t.Result;
+
+                    if (response.Success == true)
+                    {
+                        FileStream  fs = File.OpenWrite(@"d:\tk.txt");
+                        StreamWriter streamWriter = new StreamWriter(fs);
+
+                        List<Object> list = (List<Object>)response.Result;
+                        //MessageBox.Show(list.Count.ToString());
+                        Regex regex = new Regex("/@([^\"]+)");
+
+                        List<string> listTiktok = new List<string>();
+
+                        foreach (Object item in list)
+                        {
+                            if (item != null && item is string)
+                            {
+                                string tempString = item.ToString();
+                                Match m = regex.Match(tempString);
+                                if (m.Success)
+
+                                {
+                                    string value = m.Groups[0].Value.Remove(0, 2);
+                                    if (value.LastIndexOf("/live") > -1)
+                                    {
+                                        value = value.Substring(0, value.Length - 5);
+                                      
+
+                                    }
+                                    streamWriter.WriteLine(value);
+                                    listTiktok.Add(value);
+                                }
+                                else
+                                {
+                                    streamWriter.WriteLine(tempString);
+                                    listTiktok.Add(tempString);
+
+                                }
+                            }
+
+                          
+
+
+                        }
+                        streamWriter.Close();
+                        fs.Close();
+
+
+                        listBox1.DataSource = listTiktok;
+                    }
+                }
+            }, TaskScheduler.FromCurrentSynchronizationContext());
+
+        }
+
+        private void btnParse_Click_1(object sender, EventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("function tempFunction() {");
+            sb.AppendLine("     var arry=[];");
+            sb.AppendLine("     var obj=document.getElementsByClassName('tiktok-1mo2fkg-DivUserLinkContainer e797se20');");
+            sb.AppendLine("  for (var i in obj){ arry.push(obj[i].innerHTML); }");
+           
+            
+            sb.AppendLine("     return arry;");
+            sb.AppendLine("}");
+            sb.AppendLine("tempFunction();");
+
+            
+            var task = chromiumWebBrowser1.EvaluateScriptAsync(sb.ToString());
+            task.ContinueWith(t =>
+            {
+                if (!t.IsFaulted)
+                {
+                    var response = t.Result;
+
+                    if (response.Success == true)
+                    {
+                        List<Object> list = (List < Object > )response.Result;
+                        Regex regex = new Regex("/@([^\"]+)");
+                      
+                        List<string> listTiktok = new List<string>();
+                        
+                        foreach (Object item in list)
+                        {
+                            if (item != null && item is string)
+                            { 
+                                string tempString = item.ToString();
+                                Match m = regex.Match(tempString);
+                                if (m.Success)
+
+                                {
+                                    string value = m.Groups[0].Value.Remove(0,2);
+                                    if (value.LastIndexOf("/live")>-1)
+                                    {
+                                        value = value.Substring(0, value.Length - 5);
+                                    }
+                                    listTiktok.Add(value);
+                                }
+                            }
+
+
+
+                        }
+
+                       
+                        listBox1.DataSource = listTiktok;
+                    }
+                }
+            }, TaskScheduler.FromCurrentSynchronizationContext());
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ContextMenuStrip listboxMenu = new ContextMenuStrip();
+            ToolStripMenuItem rightMenu = new ToolStripMenuItem("Copy");
+            rightMenu.Click += new EventHandler(Copy_Click);
+            listboxMenu.Items.AddRange(new ToolStripItem[] { rightMenu });
+            listBox1.ContextMenuStrip = listboxMenu;
+        }
+
+        private void Copy_Click(object sender, System.EventArgs e)
+        {
+            try
+            {
+                Clipboard.SetText(listBox1.Items[listBox1.SelectedIndex].ToString());
+            }
+            catch { }
+
+        }
+    }
+}
